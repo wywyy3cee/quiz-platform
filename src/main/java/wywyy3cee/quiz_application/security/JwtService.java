1cef546aa0ba89c270dd36a8bd84ef799511db56
Vladislav Zlatov <163848800+wywyy3cee@users.noreply.github.com>
2026-06-25 23:06:11 +0300

    wip: pause work on security block, will resume later

--- src/main/java/wywyy3cee/quiz_application/security/JwtService.java
+++ src/main/java/wywyy3cee/quiz_application/security/JwtService.java
@@ -33,7 +33,7 @@ public class JwtService {
                 .getPayload();
     }
 
-    private String extractUsername(String token) {
+    public String extractUsername(String token) {
         return extractAllClaims(token).getSubject();
     }
 
